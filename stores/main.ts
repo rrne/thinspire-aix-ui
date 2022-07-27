5f37a27f952481245a418cf5ddf581bf65f0ffc0
dev-rne <coco.wapplab@gmail.com>
2022-07-27 17:12:02 +0900

    THINGSPIRE-25 JIRA connected

--- stores/main.ts
+++ stores/main.ts
@@ -6,7 +6,6 @@ const Main = observable({
   getNewsAPI() {
     axios.get("http://localhost:8000/search/news",{params: {query:"시화"}}).then((res) => {
       this.newsData = res.data.items
-      console.log(res.data);
       
     });
   },
